6459f3d11d6329c89f65aab56b12625c4515a037
cis107 <cis107src@gmail.com>
2026-04-15 05:46:53 +0000

    key fix

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY || '';
 
 // This prevents the "Key is required" crash during the build phase
 export const supabase = createClient(
